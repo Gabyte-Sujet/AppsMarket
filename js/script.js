517f2dc8d68259ce2bbb7cace533603454d6c7bf
sujet <vakogabaidze@gmail.com>
2021-05-09 18:28:23 +0400

    responsive added & fixed details

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
       dots: true,
       arrows: false,
       slidesToShow: 1,
-      // autoplay: true,
+      autoplay: true,
   });
 });
+
+var toggleButton = document.querySelector('.burger-menu');
+var nav = document.querySelector('#nav-bar');
+// var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+	toggleButton.classList.toggle('toggle');
+  nav.classList.toggle('toggle');
+});
+
+nav.addEventListener('click', function(){
+  nav.classList.toggle('toggle');
+  toggleButton.classList.toggle('toggle');
+})
